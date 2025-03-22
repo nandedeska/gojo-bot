9c1991861e796e609added104f78103cf496e3a2
unknown <miguelvisquera26@gmail.com>
2025-03-22 19:13:00 +0800

    Create EmbedData class
    This class holds all embeds and buttons needed for the combat handler.

--- Utility/CombatHandler.js
+++ Utility/CombatHandler.js
@@ -1,3 +1,11 @@
+const {
+  Client,
+  ButtonInteraction,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 const AdventureInfo = require("../Schemas/AdventureInfo");
 const StandStats = require("../Schemas/StandStats");
 
@@ -72,3 +80,94 @@ class AdventureData {
   }
 }
 
+class EmbedData {
+  fightEmbed;
+  turnEmbed;
+  opponentTurnEmbed;
+  opponentExtraTurnEmbeds;
+  quoteEmbed;
+  winEmbed;
+  rewardEmbed;
+
+  adventureButtons;
+  abilityButtons;
+
+  constructor() {}
+
+  static async new(adventureData) {
+    const data = new EmbedData();
+    await data.init(adventureData);
+    return data;
+  }
+
+  async init(adventureData) {
+    let guildId = adventureData.guildId;
+    let player = adventureData.player;
+    let opponent = adventureData.opponent;
+
+    this.fightEmbed = new EmbedBuilder()
+      .setAuthor({
+        name: `DUEL: ${player.username} vs ${opponent.displayName}`,
+      })
+      .setColor("#D31A38")
+      .setImage(
+        "https://cdn.discordapp.com/attachments/562958339034841098/1088740524342640700/image.png"
+      );
+
+    this.turnEmbed = new EmbedBuilder().setColor("#D31A38");
+    this.opponentTurnEmbed = null;
+    this.opponentExtraTurnEmbeds = [];
+
+    this.quoteEmbed = new EmbedBuilder()
+      .setColor("#80FEFF")
+      .setDescription(
+        `**"${
+          opponent.quotePool[
+            Math.floor(Math.random() * opponent.quotePool.length)
+          ]
+        }"**`
+      )
+      .setFooter({ text: `${opponent.displayName}` });
+
+    this.winEmbed = new EmbedBuilder()
+      .setColor("#D31A38")
+      .setImage(
+        "https://cdn.discordapp.com/attachments/675612229676040192/1089139526624084040/image.png"
+      );
+
+    this.rewardEmbed = new EmbedBuilder()
+      .setColor("#FFDF00")
+      .setAuthor({ name: "LOOT CRATE" })
+      .setImage(
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5851f423-3ac3-4eef-88d5-2be0fc69382b/de3ayma-b38313b3-a404-4604-91e9-c7b9908f8ad1.png/v1/fill/w_1600,h_900,q_80,strp/jojo_stand_arrow_heads_by_mdwyer5_de3ayma-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvNTg1MWY0MjMtM2FjMy00ZWVmLTg4ZDUtMmJlMGZjNjkzODJiXC9kZTNheW1hLWIzODMxM2IzLWE0MDQtNDYwNC05MWU5LWM3Yjk5MDhmOGFkMS5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.y66dqY4BgvgJUSz2mCTRTKXmvoI5yxtf9yGNV349Ls0"
+      )
+      .setDescription("CONTACT DEVELOPER: REWARD EMBED ERROR");
+
+    this.adventureButtons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("Attack")
+        .setCustomId(`Adventure-Attack-${guildId}-${player.id}-${opponent.id}`)
+        .setStyle(ButtonStyle.Success),
+      new ButtonBuilder()
+        .setLabel("Dodge")
+        .setCustomId(`Adventure-Dodge-${guildId}-${player.id}-${opponent.id}`)
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setLabel("Surrender")
+        .setCustomId(
+          `Adventure-Surrender-${guildId}-${player.id}-${opponent.id}`
+        )
+        .setStyle(ButtonStyle.Danger)
+    );
+
+    this.abilityButtons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("Ability")
+        .setCustomId("Dummy")
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(true)
+    );
+  }
+}
+
+export { AdventureData, EmbedData };
