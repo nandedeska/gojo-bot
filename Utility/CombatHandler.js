4068d2ba2ce6b38ce7cd12af3aa8ff1b68f7795f
unknown <miguelvisquera26@gmail.com>
2025-04-01 16:04:12 +0800

    Add missing cooldown texts from DuelData

--- Utility/CombatHandler.js
+++ Utility/CombatHandler.js
@@ -162,6 +162,9 @@ class DuelData {
       this.attackRollHeight = this.savedData.AttackRollHeight;
       this.timeStopTurns = this.savedData.TimeStopTurns;
     }
+
+    this.challengerCooldownText = "";
+    this.challengedCooldownText = "";
   }
 }
 
